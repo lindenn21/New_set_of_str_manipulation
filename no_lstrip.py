14d00592a147e85dca3860cac90c1398725f9e07
Linden <lindenpowell218@gmail.com>
2025-03-28 19:44:18 +0800

    Fixed spacings: no_lstrip()

--- no_lstrip.py
+++ no_lstrip.py
@@ -1,6 +1,7 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 # Ask user to input
 # Remove spaces then print
+
 whitespace = 0
 text = str(input("Enter your name with spaces in the beginning: "))
 if text[0] == " ":
@@ -8,6 +9,5 @@ if text[0] == " ":
         whitespace += 1
 
 text = text[whitespace:]
-
 
 print(text)
