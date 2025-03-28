0119997c80fb5c48be1c59bcc8453a48e6bdc768
Linden <lindenpowell218@gmail.com>
2025-03-28 21:00:17 +0800

    Starting no_removeprefix.py, comment n text prefix added

---
+++ no_removeprefix.py
@@ -0,0 +1,7 @@
+# Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+# Ask user to input
+# If user inputs a word
+# Remove PREFIX
+
+text = str(input("Enter: "))
+prefix = "HELLO"
