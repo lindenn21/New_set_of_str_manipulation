a6545686583eb94bb8a30401a98070db4ad8033d
Linden <lindenpowell218@gmail.com>
2025-03-28 19:21:53 +0800

    Testing

--- no_lstrip.py
+++ no_lstrip.py
@@ -1,7 +1,7 @@
 # Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 # Ask user to input
 # Remove spaces then print
-
+whitespace = 0
 text = str(input("Enter your name with spaces in the beginning: "))
 text = text.replace(" ", "")
 print(text)
