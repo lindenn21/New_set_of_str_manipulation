b412d0f394a9dbdc28d03c63891624b725ba6762
Linden <lindenpowell218@gmail.com>
2025-03-28 19:18:24 +0800

    First commit: no_lstrip is starting, added comments

---
+++ no_lstrip.py
@@ -0,0 +1,4 @@
+# Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+# Ask user to input
+# Remove spaces then print
+
