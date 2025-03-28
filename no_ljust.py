361b97c169c4613d8d32d560209e3d83a7396924
Linden <lindenpowell218@gmail.com>
2025-03-29 00:14:26 +0800

    ljust alternative

---
+++ no_ljust.py
@@ -0,0 +1,5 @@
+# Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+# Ask user to input whatever
+# Ask user how far the space should be
+# print
+
