9cb9396bb3233cf22f42a4814f7e877830e937f3
Linden <lindenpowell218@gmail.com>
2025-03-28 21:43:49 +0800

    isupper starting, comment and input done

---
+++ not_isupper.py
@@ -0,0 +1,5 @@
+# Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+# Ask user to input something
+# If its all upper print true
+
+text = str(input("enter: "))
