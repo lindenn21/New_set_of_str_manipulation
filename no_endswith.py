848868f3c0496c7b08fb2cf0ab2ffb4c4d036cb3
Linden <lindenpowell218@gmail.com>
2025-03-28 23:47:38 +0800

    Starting no_endswith.py

---
+++ no_endswith.py
@@ -0,0 +1,7 @@
+# Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+# Ask user to input a word
+# Check if the word ends with e
+# Print true if there is, false if there aren't any
+
+
+
