2ae30d9d501f4bc00d99bc7698576587d4d2da0a
Linden <lindenpowell218@gmail.com>
2025-03-30 03:16:45 +0800

    No_swapcase

---
+++ no.swapcase().py
@@ -0,0 +1,5 @@
+# Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+# Ask user to input something
+# Change small letters to capital and capital to small leters
+# print
+
