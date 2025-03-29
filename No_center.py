9b60952164b55f32e3a6a2a1397f6baae0c60f67
Linden <lindenpowell218@gmail.com>
2025-03-29 11:26:36 +0800

    Renamed No_ljust.py, starting No_center.py

---
+++ No_center.py
@@ -0,0 +1,6 @@
+# Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+# Ask user to input something
+# Specify the amount of spaces needed
+# Center it based on the amount
+# Print
+
