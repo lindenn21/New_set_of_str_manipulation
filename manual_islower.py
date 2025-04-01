52140ecffe4120b0b19f021ca6f9ffd189a8dde8
Linden <lindenpowell218@gmail.com>
2025-04-01 19:04:14 +0800

    starting manual_islower

---
+++ manual_islower.py
@@ -0,0 +1,5 @@
+# Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+# Ask user to input
+# If input is lower, print true
+
+text = input("Enter string, this will check if the string you enter is lower case: ")
