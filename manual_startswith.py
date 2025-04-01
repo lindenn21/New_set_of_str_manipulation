c80aabeefbf00ba455f97b867267089ce4297ad4
Linden <lindenpowell218@gmail.com>
2025-04-01 19:09:40 +0800

    manual_startswith is starting

---
+++ manual_startswith.py
@@ -0,0 +1,5 @@
+# Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+# Ask user to input something
+# If the string starts with HELLO, print true
+
+text = input("Enter a string, this will check if you start with HELLO: ")
