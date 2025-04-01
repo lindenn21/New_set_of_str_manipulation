a8087d986557cba2301b391d0c254b15a789a11b
Linden <lindenpowell218@gmail.com>
2025-04-01 19:16:15 +0800

    starting Manual_rjust

---
+++ Manual_rjust.py
@@ -0,0 +1,6 @@
+# Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+# Ask user to input
+# Add space at the beginning
+# print
+
+text = input("Enter your name: ")
