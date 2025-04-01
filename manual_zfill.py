0b45e1e062eaa468135a563d58e2c5e3254dc53b
Linden <lindenpowell218@gmail.com>
2025-04-01 19:24:23 +0800

    starting manual_zfill

---
+++ manual_zfill.py
@@ -0,0 +1,6 @@
+# Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+# Ask user to input something
+# Ak the user to also add the number of zeroes they'd like
+# Print the string with zeroes at the beginning
+
+text = input("Enter a string:")
