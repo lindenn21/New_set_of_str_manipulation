ac8f65548ac0b22b800bc395d611f56e9677e57d
Linden <lindenpowell218@gmail.com>
2025-04-01 18:30:23 +0800

    no_removesuffix

---
+++ without_removesuffix.py
@@ -0,0 +1,6 @@
+# Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+# Ask user to input smth
+# remove suffix, but I can't use removesuffix()
+# print
+
+text = input("Enter:")
