f16713546278a712585f7e4fc720673b50c922cd
Linden <lindenpowell218@gmail.com>
2025-04-01 17:55:00 +0800

    starting rstip

---
+++ Without_rstip.py
@@ -0,0 +1,7 @@
+# Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+# Ask user to input something with spaces at the end
+# specify the whitespace to be at the end by using len(text) - 1
+# remove whitespace
+# print
+
+text = input("Enter: ")
