f6a17cb791eb4a0e01ac400bd7719195fd4dc68a
Linden <lindenpowell218@gmail.com>
2025-04-01 18:54:50 +0800

    Starting without_upper

---
+++ Without_upper.py
@@ -0,0 +1,7 @@
+# Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+# Ask user to print something
+# Lower the string]
+# Swapcase the lowered string
+# print
+
+text = input("Enter: ")
