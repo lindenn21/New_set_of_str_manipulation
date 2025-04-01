3574602c39ddd079e87246ca5cb13f6e65caac1d
Linden <lindenpowell218@gmail.com>
2025-04-01 19:38:11 +0800

    starting Manual_count

---
+++ Manual_count.py
@@ -0,0 +1,4 @@
+# Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+# Ask user to input
+# Count how many times the same thing appears
+
